113f04e83a99fb24b2dccbacace864c7f2de84f4
tiffany <ting11222001@gmail.com>
2024-04-19 18:46:10 +0930

    add readme.md and added fluent assertions library

---
+++ README.md
@@ -0,0 +1,15 @@
+# Unit-Test-C#
+
+## First Unit Test
+
+Written the first unit test demonstrating the basic unit test concept.
+- UnitTests > Program.cs
+
+
+## Network Utility
+
+- NetworkUtility > Ping > NetworkService.cs
+- NetworkUtility.Tests > PingTests > NetworkServiceTests.cs
+
+Installed Fluent Assertions package from Nuget in the `NetworkUtility.Tests` folder.
+
---
+++ UnitTests/NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        // ClassName_MethodName_Result
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange - variables, classes, mocks
+            var pingService = new NetworkService();
+
+            // Act
+            var result = pingService.SendPing();
+
+            // Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+
+
+        }
+    }
+}
